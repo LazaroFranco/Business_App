288702cce01f146942ac55415801fb7351304801
Lazaro Franco <cubanboymodz@gmail.com>
2020-02-26 08:58:40 -0500

    pages created

---
+++ app/about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <title>Welcome!</title>
+  </head>
+  <body>
+
+    <section class="centered" id="header">
+        <h1 class="header-h1">Man-A-Biz</h1>
+        <section class="sign">
+            <a href="register.php">Register</a>
+            <a href="login.php">Login</a>
+        </section>
+    </section>
+     <nav>
+         <ul>
+             <li><a href="/Man-A-Biz/app/index.php">Home</a></li>
+             <li><a href="">PAGE 1</a></li>
+             <li><a href="">PAGE 2</a></li>
+             <li><a href="/Man-A-Biz/app/about.php">About Us</a></li>
+         </ul>
+     </nav>
+
+    <section class="container">
+
+<h2>What We Have To Offer</h2>
+<p><img src="https://dummyimage.com/250/ffffff/000000">
+Lorem ipsum dolor sit amet, consect
+etur adipiscing elit. Phasellus imperdiet, nulla e
+t dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestib
+ulum augue u
+t aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in
+ odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed or
+
+nare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit.
+</p>
+</section>
+
+<section class="info">
+<h2>What We Have To Offer</h2>
+<p>
+<img class="right" src="https://dummyimage.com/250/ffffff/000000">
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit.
+</p>
+</section>
+
+<section class="info">
+<h2>What We Have To Offer</h2>
+<p><img src="https://dummyimage.com/250/ffffff/000000">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit.
+</p>
+</section
+
+    </section>
+<?php
+
+?>
+    <footer>
+      <h2 class="footer-h1">Man-A-Biz</h2>
+      <address>
+        <p>750 E King St</p>
+        <p>Lancaster, PA 17602</p>
+        <p><a href="tel:1-717-299-7701" class="phone">717.299.7701</a></p>
+      </address>
+    </footer>
+  </body>
+</html>
---
+++ app/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <title>Welcome!</title>
+  </head>
+  <body>
+
+    <header class="centered" id="header">
+       <img class="hero-img" src="https://za.dynabook.com/images/local/en_EU/Toshibytes/images/future_of_business.jpeg">
+        <section class="sign">
+          <a class="a" href="register.php">Register</a>
+          <a class="a" id="login" href="login.php">Login</a>
+       </section>
+     <h1 class="header-h1">Man-A-Biz</h1>
+          </header>
+
+     <nav>
+         <ul>
+             <li><a href="/Man-A-Biz/app/index.php">Home</a></li>
+             <li><a href="">PAGE 1</a></li>
+             <li><a href="">PAGE 2</a></li>
+             <li><a href="/Man-A-Biz/app/about.php">About Us</a></li>
+         </ul>
+     </nav>
+
+     <section class="hero-img"></section>
+
+<section class="home">
+<h2>What We Have To Offer</h2>
+<p><img src="https://dummyimage.com/250/ffffff/000000">
+Lorem ipsum dolor sit amet, consect
+etur adipiscing elit. Phasellus imperdiet, nulla e
+t dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestib
+ulum augue u
+t aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in
+ odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed or
+
+nare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit.
+</p>
+
+    </section>
+<?php
+
+?>
+    <footer>
+      <h2 class="footer-h1">Man-A-Biz</h2>
+      <address>
+        <p>750 E King St</p>
+        <p>Lancaster, PA 17602</p>
+        <p><a href="tel:1-717-299-7701" class="phone">717.299.7701</a></p>
+      </address>
+    </footer>
+  </body>
+</html>
