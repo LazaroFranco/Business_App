5a7cb8c7ca9dda1db75fd04f6be77a54455c4efc
Alaa Ayyash <aja5063@gmail.com>
2020-02-28 10:54:06 -0500

    create registration employee approval file

--- app/db.php
+++ app/db.php
@@ -1,8 +1,8 @@
 <?php
   $dbServername = "localhost";
   $dbUsername = "root";
-  $dbPassword = "password";
-  $dbName = "our db name here..";
+  $dbPassword = "";
+  $dbName = "businessManagement";
 
   $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
