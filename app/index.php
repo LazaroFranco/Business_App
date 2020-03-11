2d06283a0d8f2c430c5d1b770146fc80b4073bda
Lazaro Franco <cubanboymodz@gmail.com>
2020-03-11 10:26:48 -0400

    modifying navbar

--- app/index.php
+++ app/index.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+include_once 'db.php';
+  if (isset($_SESSION['ID'])){
+    // Start looking for roles.
+    $role= $_SESSION['role'];
+    // echo "Hello, " . $_SESSION['firstName'] . "! You are a " . $role . ".";
+    if ($role == "admin"){
+      // Create the Admin Home.
+        include("./admin/admin_home.php");
+      } else if ($role == "owner"){
+        // Create the Owner Home.
+          include("./owner/owner.php");
+        } else if ($role == "supervisor"){
+          // Create the supervisor Home.
+            include("./supervisor/supervisor_home.php");
+          } else if ($role == "employee"){
+            // Create the employee Home.
+              include("./employee/employee_home.php");
+            }
+  } else {
+    // They ain't even logged in.
+    header("Location: index.php");
+  }
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
