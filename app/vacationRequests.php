dc89b53ba3105148eddf4c8bee1824507cd6eed4
Alaa Ayyash <aja5063@gmail.com>
2020-03-18 11:22:43 -0400

    add php to send data in form  to db

--- app/vacationRequests.php
+++ app/vacationRequests.php
@@ -20,11 +20,12 @@ if (!$conn) {
         <h2 class="header-h1">Vacation Request Form</h1>
 
         <form action="vacationRequests.php" name="vacReq" method="POST">
-            <label>Current Date:</label><input type="date" name="date"/><br>
+            <label>Current Date:</label><input type="date" name="curentDate"/><br>
             <label>Employee First Name:</label><input type="text" name="fname"/><br>
             <label>Employee Last Name:</label><input type="text" name="lname"/><br>
             <label>Type of Request (Vacation/Sick):</label><input type="text" name="typeOfReq"/><br>
-            <label>Paid</label><input type="checkbox" name="paid"/><label>Unpaid</label><input type="checkbox" name="unpaid"/><br>
+            <label>Paid </label><input type="radio" name="paid"/><br>
+            <label>Unpaid </label><input type="radio" name="paid"/><br>
             <label>Comments:</label><input type="text" name="comments"/><br>
             <label>Start Date:</label><input type="date" name="startDate"/><br>
             <label>End Date:</label><input type="date" name="endDate"/><br>
@@ -32,3 +33,25 @@ if (!$conn) {
         </form>
     </body>
 </html>
+
+<?php
+if (isset($_POST['vacReq'])) {
+    $currentDate = $_POST['currentDate'];
+    $firstName = $_POST['fname'];
+    $lastName = $_POST['lname'];
+    $typeOfRequest = $_POST['typeOfReq'];
+    $paidOrUnpaid = $_POST['paid'];
+    $comments = $_POST['comments'];
+    $startDate = $_POST['startDate'];
+    $endDate = $_POST['endDate'];
+
+    if (($currentDate != '') && ($firstName != '') && ($lastName != '') && ($typeOfRequest != '') && ($paidOrUnpaid != '') && ($startDate != '') && ($endDate !='')) {
+        $insertRequest = "INSERT INTO `Vac_Req_Form` (Current_Date, Fname, Lname, Type_Of_Request, Paid, Comments, Start_Date_Requested, End_Date_Requested, Approved) VALUES ('$currentDate', '$firstName', '$lastName', '$typeOfRequest', '$paidOrUnpaid', '$phonenumber', '$comments', '$startDate', '$endDate')";
+        mysqli_query($conn, $insertRequest);
+        echo "You have submitted a request for time off.";
+    } else {
+        echo "Error with submitting request." . mysqli_error($conn);
+    }
+}
+
+?>
