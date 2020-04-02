8f1a8882d169fc750e11d3f54b240e6e3d36127e
Lazaro Franco <cubanboymodz@gmail.com>
2020-04-02 00:45:17 -0400

    Fixed form added styles

--- app/vacationRequests.php
+++ app/vacationRequests.php
@@ -39,11 +39,11 @@ if (!$conn) {
              <label>
                <h2>Paid/Unpaid:</h2>
                 <label>Paid
-                <input type="radio" id="paid" name="paidStatus" value="paid"/>
+                <input type="radio" id="paid" name="paidStatus" value="paid" />
                 </label>
                 <br>
                 <label>Unpaid
-                <input type="radio" id= "unpaid" name="paidStatus" value="unpaid"/>
+                <input type="radio" id= "unpaid" name="paidStatus" value="unpaid" />
                 </label>
               </label>
             </section>
@@ -52,17 +52,23 @@ if (!$conn) {
             <input type="text" name="comments"/>
             <br>
             <label>Start Date:</label>
-            <input type="date" name="startDate"/>
+            <input type="date" name="startDate" required/>
             <br>
             <label>End Date:</label>
-            <input type="date" name="endDate"/>
+            <input type="date" name="endDate" required/>
             <br>
             <input class="vrsubmit" type="submit" name="submit" id="submit" value="Submit Request">
         </form>
         <form action="vacationRequests.php" name="viewReqs" method="POST">
-            <input type="submit" id="submit" name="vacReqForms" value="View Employee Requests">
         </form>
       </div>
+      <a id="submit" style='cursor:grab;' onclick="all_show()">
+        View All Requests
+      </a>
+
+      <a id="submit" style='cursor:grab;' onclick="approved_show()">
+        View Upcoming Vacations (Approved)
+      </a>
 <?php
 //if (isset($_GET['login'])) {
     if (isset($_POST['submit'])) {
@@ -108,10 +114,9 @@ if (!$conn) {
         }
     }
 //}
-if (isset($_POST['vacReqForms'])) {
     $getRequestList = mysqli_query($conn, "SELECT * FROM Vac_Req_Form WHERE Approved=0");
     $i = 1;
-    echo "<form class='vrform2' action='vacationRequests.php' method='POST'>";
+    echo "<form style='display:none;' id='vrform2' class='vrform2' action='vacationRequests.php' method='POST'>";
         echo "<h1>Employee Requests for Time Off</h1>";
             echo "<table class='content-table'>";
               echo "<thead>";
@@ -154,8 +159,8 @@ if (isset($_POST['vacReqForms'])) {
                         mysqli_query($conn, $approveRequest);
                     }
                 }
-                header('Location: vacationRequests.php');
             }
+
             if (isset($_POST['deny'])) {
                 if (isset($_POST['check'])) {
                     foreach ($_POST['check'] as $value) {
@@ -163,11 +168,77 @@ if (isset($_POST['vacReqForms'])) {
                         mysqli_query($conn, $deleteRequest);
                     }
                 }
-                header('Location: vacationRequests.php');
             }
-        }
-        include 'footer.php';
-        mysqli_close($conn);
+
         ?>
+
+        <div id="rowmargin" class="row">
+          <div class="row">
+            <div class="boxx" style="display:none;" id="showApproved">
+                <h2>Upcoming Vacations</h2>
+
+              <table class='content-table'>
+                <?php
+
+                $userQuery = mysqli_query($conn, "SELECT * FROM Vac_Req_Form
+                WHERE Approved=1;");
+                $i = 1;
+
+                    echo "<thead>";
+                      echo "<tr>";
+                          echo "<th>Employee ID</th>";
+                          echo "<th>Employee Name</th>";
+                          echo "<th>Paid Status</th>";
+                          echo "<th>Comments</th>";
+                          echo "<th>Start Date</th>";
+                          echo "<th>End Date</th>";
+                      echo "</tr>";
+                    echo "</thead>";
+
+                  while ($row =  mysqli_fetch_array($userQuery)) {
+
+                  $startDate = date("d-m-Y", strtotime($row['Start_Date_Requested']));
+
+                  $endDate = date("d-m-Y", strtotime($row['End_Date_Requested']));
+
+                    echo "<tbody>";
+                    echo "<tr>";
+                    echo "<td>".$row['Employee_ID']."</td>";
+                     echo "<td>".$row['Fname']. " " .$row['Lname']."</td>";
+                     echo '<td>' . ($row['Paid'] ? 'Paid' : 'Not Paid') . '</td>';
+                      echo "<td>" . $row['Comments'] . "</td>";
+
+                     echo "<td>".$startDate."</td>";
+                     echo "<td>".$endDate."</td>";
+
+                     echo "</tr>";
+                    echo "</tbody>";
+                    $i++;
+                }
+
+                mysqli_close($conn);
+
+                ?>
+              </table>
+            </div>
+          </div>
+        </div>
+        <?php
+        include 'footer.php';
+         ?>
     </body>
+    <script>
+      function all_show() {
+        document.getElementById('vrform2').style.display = "block";
+
+        document.getElementById('showApproved').style.display = "none";
+
+      }
+      function approved_show() {
+        document.getElementById('showApproved').style.display = "block";
+
+        document.getElementById('vrform2').style.display = "none";
+      }
+
+    </script>
 </html>
