42b2a3ffa6001d68c6bc4a8e511cfd67cbb39c58
lazarofranco <lazaro300x@icloud.com>
2022-12-15 18:28:26 -0500

    db updated

--- app/db.php
+++ app/db.php
@@ -1,9 +1,8 @@
 <?php
   $dbServername = "remotemysql.com";
   $dbUsername = "g9tOkevXAh";
-  $dbPassword = "HFmMNoAorw";
+  $dbPassword = "8WpOOAyTcC";
   $dbName = "g9tOkevXAh";
 
   $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
-
