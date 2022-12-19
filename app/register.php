00adb606f2f9e486c3abc4281f29c4c84a44d998
lazarofranco <lazaro300x@icloud.com>
2022-12-19 18:22:07 -0500

    number edit

--- app/register.php
+++ app/register.php
@@ -258,7 +258,7 @@ if($role == "1" & $fname != "" & $lname != "" & $email != "" & $phone != "" & $p
   return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
     3,
     6
-  )}-${phoneNumber.slice(6, 9)}`;
+  )}-${phoneNumber.slice(6, 10)}`;
 }
 
 function phoneNumberFormatter() {
