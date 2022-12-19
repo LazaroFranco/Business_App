c55737c714c785c40e068ad2ceb00f1edb85bf6f
lazarofranco <lazaro300x@icloud.com>
2022-12-19 13:07:54 -0500

    reg

--- app/register.php
+++ app/register.php
@@ -1,6 +1,6 @@
 <?php
-require "db.php";
-session_start();
+include_once 'db.php';
+
 ?>
 
 <script LANGUAGE="JavaScript">
@@ -19,17 +19,16 @@ session_start();
     }
       function mployee(emp){
         if (emp.value == "1"){
-        document.getElementById("basic").style.display = "inline-grid";
+        document.getElementById("basic").style.display = "block";
         document.getElementById("buss_owner").style.display = "none";
         document.getElementById("button").style.display = "inline-grid";
-        document.getElementById("section").style.display = "inline-grid";
 
     }
     if (emp.value == "0"){
-        document.getElementById("basic").style.display = "none";
-        document.getElementById("buss_owner").style.display = "inline-grid";
+        document.getElementById("basic").style.display = "block";
+        document.getElementById("buss_owner").style.display = "block";
         document.getElementById("button").style.display = "inline-grid";
-        document.getElementById("section").style.display = "inline-grid";
+
     }
       }
 
@@ -56,25 +55,25 @@ session_start();
   <body>
        <div class="bg" ></div>
 
-    <header class="centered" id="header">
-       <h1 class="header-h1">Man-a-Biz</h1>
-       <h2 class="text-center mt-0">Register</h2>
+       <header class="centered" id="header">
+          <h1 class="header-h1">Man-a-Biz</h1>
+          <h2 class="text-center mt-0">Register</h2>
 
-        <section class="sign">
-          <!--
-          <a class="a" onclick="myFunction('Demo1')"  class="w3-btn w3-block w3-black w3-left-align" id="hide" >Login</a>
-        -->
-          <a class="a" href="index.php">Home</a>
-          <div id="Demo1" class="w3-container w3-hide">
-            <label for="uname"><b>Username</b></label>
-              <input type="text" placeholder="Enter Username" name="uname" required>
+           <section class="sign">
+             <!--
+             <a class="a" onclick="myFunction('Demo1')"  class="w3-btn w3-block w3-black w3-left-align" id="hide" >Login</a>
+           -->
+             <a class="a" href="index.php">Home</a>
+             <div id="Demo1" class="w3-container w3-hide">
+               <label for="uname"><b>Username</b></label>
+                 <input type="text" placeholder="Enter Username" name="uname" required>
 
-            <label for="psw"><b>Password</b></label>
-               <input type="password" placeholder="Enter Password" name="psw" required>
-            <a class="a" onclick="myFunction('Demo1')" class="w3-btn w3-block w3-black w3-left-align" id="login">Login</a>
-            <a class="a" onClick="history.go(0);">Cancel</a>
-          </div>
-    </header>
+               <label for="psw"><b>Password</b></label>
+                  <input type="password" placeholder="Enter Password" name="psw" required>
+               <a class="a" onclick="myFunction('Demo1')" class="w3-btn w3-block w3-black w3-left-align" id="login">Login</a>
+               <a class="a" onClick="history.go(0);">Cancel</a>
+             </div>
+       </header>
     <h3>What is your role?</h3>
   <form>
     <input type="radio" name="type-of-user" id="owner" onclick="mployee(this)" value="0"required>
@@ -99,139 +98,108 @@ session_start();
       <label for="BPhone" class="">Business Phone (Format: 000-000-0000)</label><input class="" type="tel" name="BPhone" pattern="[[0-9]{3}-[0-9]{3}-[0-9]{4}"><br>
 
 </div>
-<div>
+
 <input style="display: none;" id="button" type="submit" name="submit" value="Submit">
-<br>
-<p style="display: none;" id="section">
-Thank you for information, we will contact you within 48 hours
-</p>
-</div>
-
   <form>
-    <?php
+<?php
+if(isset($_GET['submit'])){
+  $role = $_GET['type-of-user'];
 
-    if(isset($_GET['submit'])){
-
-      $role = $_GET['type-of-user'];
-
-      $fname = $_GET['Fname'];
-      $lname = $_GET['Lname'];
-      $email = $_GET['Email'];
-      $phone = $_GET['Phone'];
-      $birth = $_GET['Birth'];
-      $password = $_GET['Pword'];
-      $compName = $_GET['CompName'];
-      $baddress = $_GET['BAddress'];
-      $city = $_GET['City'];
-      $state = $_GET['State'];
-      $bphone = $_GET['BPhone'];
-      $Ccode = $_GET['Ccode'];
-      $compID = "No";
-
-      if(($role == "0" & $fname != "" & $lname != "" & $email != "" & $phone != ""
-    & $password != "" & $birth != "" & $compName != "" & $baddress != "" & $city != "" & $state != "" & $bphone != "" & $Ccode != "")){
-      $sql = "INSERT INTO `Company`(Business_Name, Business_Address, Phone, Email, City, State, Company_Code) VALUES ('$compName','$baddress','$bphone','$email','$city','$state','$Ccode')";
-      mysqli_query($conn,$sql);
-
-      $UserCompanyID = "SELECT ID FROM `Company` WHERE Company_Code = '$Ccode'";
-      $result = mysqli_query($conn, $UserCompanyID);
-
-      $resultCheck = mysqli_num_rows($result);
-      // RESULT CHECK SHOWS RESULT ID
-      //  $resultCheck;
-       "<p>Submission was succesful, please wait up to 48hrs for approval.</p>";
-      if($resultCheck > 0){
-        while($row = mysqli_fetch_assoc($result)){
-        $compID = $row['ID'];
-      }
-      }
+  $fname = $_GET['Fname'];
+  $lname = $_GET['Lname'];
+  $email = $_GET['Email'];
+  $phone = $_GET['Phone'];
+  $birth = $_GET['Birth'];
+  $password = $_GET['Pword'];
+  $compName = $_GET['CompName'];
+  $baddress = $_GET['BAddress'];
+  $city = $_GET['City'];
+  $state = $_GET['State'];
+  $bphone = $_GET['BPhone'];
+  $Ccode = $_GET['Ccode'];
+  $compID = "No";
+  $employID = "No";
 
 
-      $sql = "INSERT INTO `Users`(Fname, Lname, Phone, Email, Password, DoB, Company_Code, Approved, Type_Of_User, Company_ID) VALUES ('$fname','$lname','$phone','$email','$password','$birth', '$Ccode', '0','$role','$compID')";
-      mysqli_query($conn,$sql);
+  if(($role == "0" & $fname != "" & $lname != "" & $email != "" & $phone != ""
+& $password != "" & $birth != "" & $compName != "" & $baddress != "" & $city != "" & $state != "" & $bphone != "" & $Ccode != "")){
+  $sql = "INSERT INTO `Company`(Business_Name, Business_Address, Phone, Email, City, State, Company_Code) VALUES ('$compName','$baddress','$bphone','$email','$city','$state','$Ccode')";
+  mysqli_query($conn,$sql);
 
-      $EmpID = "SELECT ID FROM `Users` WHERE Email = '$email'";
-      $result = mysqli_query($conn, $EmpID);
+  $UserCompanyID = "SELECT ID FROM `Company` WHERE Company_Code = '$Ccode'";
+  $result = mysqli_query($conn, $UserCompanyID);
 
-      $resultCheck = mysqli_num_rows($result);
-      // RESULT CHECK SHOWS RESULT ID
-      //  $resultCheck;
-       "<p>Submission was succesful, please wait up to 48hrs for approval.</p>";
-      if($resultCheck > 0){
-        while($row = mysqli_fetch_assoc($result)){
-        $EmpID = $row['ID'];
-      }
-      }
-
-
-      $sql = "INSERT INTO `Employees`(Company_ID, Emp_ID, Authorization) VALUES ('$compID', '$EmpID', 'Secretary')";
-      mysqli_query($conn,$sql);
-      ?>
-      <!--
-      <script type="text/javascript">
-      window.location.href = 'https://business-app.herokuapp.com/app/';
-      </script>
-
-    -->
-      <?php
-    }
-    if($role == "1" & $fname != "" & $lname != "" & $email != "" & $phone != "" & $password != "" & $birth != ""){
-
-      $UserCompanyID = "SELECT ID FROM `Company` WHERE Company_Code = '$Ccode'";
-      $result = mysqli_query($conn, $UserCompanyID);
-
-      $resultCheck = mysqli_num_rows($result);
-      // RESULT CHECK SHOWS RESULT ID
-      //  $resultCheck;
-       "<p>Submission was succesful, please wait up to 48hrs for approval.</p>";
-      if($resultCheck > 0){
-        while($row = mysqli_fetch_assoc($result)){
-        $compID = $row['ID'];
-      }
-      }
+  $resultCheck = mysqli_num_rows($result);
+  if($resultCheck > 0){
+    while($row = mysqli_fetch_assoc($result)){
+    $compID = $row['ID'];
+  }
+}
 
 
-      $sql = "INSERT INTO `Users`(Company_ID, Fname, Lname, Phone, Email, Password, DoB, Approved, Type_Of_User, Company_Code) VALUES ('$compID', '$fname','$lname','$phone','$email','$password','$birth','0','$role','$Ccode')";
-      mysqli_query($conn,$sql);
+  $sql = "INSERT INTO `Users`(Fname, Lname, Phone, Email, Password, DoB, Company_Code, Approved, Type_Of_User, Company_ID) VALUES ('$fname','$lname','$phone','$email','$password','$birth', '$Ccode', '0','$role','$compID')";
+  mysqli_query($conn,$sql);
 
-      $EmpID = "SELECT ID FROM `Users` WHERE Email = '$email'";
-      $result = mysqli_query($conn, $EmpID);
+  $EmpID = "SELECT ID FROM `Users` WHERE Email = '$email'";
+  $result = mysqli_query($conn, $EmpID);
 
-      $resultCheck = mysqli_num_rows($result);
-      // RESULT CHECK SHOWS RESULT ID
-      // echo $resultCheck;
-      echo "<p>Submission was succesful, please wait up to 48hrs for approval.</p>";
-      if($resultCheck > 0){
-        while($row = mysqli_fetch_assoc($result)){
-        $EmpID = $row['ID'];
-      }
-      }
-
-      $sql = "INSERT INTO `Employees`(Company_ID, Emp_ID, Authorization) VALUES ('$compID', '$EmpID', 'Employee')";
-      mysqli_query($conn,$sql);
-
-      ?>
-      <!--
-      <script type="text/javascript">
-      window.location.href = 'https://business-app.herokuapp.com/app/';
-      </script>
-    -->
-      <?php
-
-    }
+  $resultCheck = mysqli_num_rows($result);
+  if($resultCheck > 0){
+    while($row = mysqli_fetch_assoc($result)){
+    $employID = $row['ID'];
+  }
+  echo "<p>Submission was succesful, please wait up to 48hrs for approval.</p>";
+}
 
 
 
-    }
-    ?>
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- SimpleLightbox plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
-<!-- Core theme JS-->
-<script src="js/scripts.js"></script>
-<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+  $sql = "INSERT INTO `Employees`(Company_ID, Emp_ID, Authorization) VALUES ('$compID', '$employID', 'Secretary')";
+  mysqli_query($conn,$sql);
+  ?>
 
+  <?php
+}
+if($role == "1" & $fname != "" & $lname != "" & $email != "" & $phone != "" & $password != "" & $birth != ""){
+
+  $UserCompanyID = "SELECT ID FROM `Company` WHERE Company_Code = '$Ccode'";
+  $result = mysqli_query($conn, $UserCompanyID);
+
+  $resultCheck = mysqli_num_rows($result);
+  if($resultCheck > 0){
+    while($row = mysqli_fetch_assoc($result)){
+    $compID = $row['ID'];
+  }
+}
+
+
+  $sql = "INSERT INTO `Users`(Company_ID, Fname, Lname, Phone, Email, Password, DoB, Approved, Type_Of_User, Company_Code) VALUES ('$compID', '$fname','$lname','$phone','$email','$password','$birth','0','$role','$Ccode')";
+  mysqli_query($conn,$sql);
+
+  $EmpID = "SELECT ID FROM `Users` WHERE Email = '$email'";
+  $result = mysqli_query($conn, $EmpID);
+
+  $resultCheck = mysqli_num_rows($result);
+  if($resultCheck > 0){
+    while($row = mysqli_fetch_assoc($result)){
+    $employID = $row['ID'];
+  }
+  echo "<p>Submission was succesful, please wait up to 48hrs for approval.</p>";
+}
+
+
+  $sql = "INSERT INTO `Employees`(Company_ID, Emp_ID, Authorization) VALUES ('$compID', '$employID', 'Employee')";
+  mysqli_query($conn,$sql);
+
+  ?>
+
+  <?php
+
+}
+
+
+
+}
+?>
 <script>
   function myFunction(id) {
     var x = document.getElementById(id);
@@ -244,11 +212,11 @@ Thank you for information, we will contact you within 48 hours
     }
   }
 </script>
-  </body>
-  <footer class="bg-light py-5">
-    <div>
-      <div>&copy;<script>document.write(new Date().getFullYear());</script>, Man-A-Biz. Property of Tech by Laz, LLC.
-      </div>
+</body>
+<footer class="bg-light py-5">
+  <div>
+    <div>&copy;<script>document.write(new Date().getFullYear());</script>, Man-A-Biz. Property of Tech by Laz, LLC.
     </div>
-  </footer>
+  </div>
+</footer>
 </html>
